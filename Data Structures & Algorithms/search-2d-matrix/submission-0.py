ad222a9fe2e241a2531051f533603697fee0b5de
cy-katryn <144767719+cy-katryn@users.noreply.github.com>
2026-07-13 21:46:09 +0800

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row = len(matrix)
+        col = len(matrix[0])    
+        
+        # Calculate the half point like normally
+        left = 0
+        right = row * col - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2 # calculate midpoint
+            i = mid // col
+            j = mid % col
+
+            if matrix[i][j] < target:
+                left = mid + 1
+            elif matrix[i][j] > target:
+                right = mid - 1
+            else:
+                return True
+        
+        return False
+
+        # Need to map half point to the actual position
